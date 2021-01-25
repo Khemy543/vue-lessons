6eddfb39f8262293982ac74bf86edc53ccbcd8c1
Khemy543 <gassafuah@gmail.com>
2021-01-25 20:13:36 +0000

    first commit

---
+++ firstVueFile.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hello">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data(){
+            return {
+                msg : "Welcome to Your Vue.js App"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .hello{
+        color:red;
+    }
+</style>
